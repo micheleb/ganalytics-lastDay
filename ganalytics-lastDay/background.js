ca9a9eeab17071f73e0a681ec42ad097d063ccde
Michele Bonazza <michele.bonazza@gmail.com>
2015-05-15 14:29:24 +0200

    Removed jQuery
    The extension would often require a manual page refresh due to errors in the shared.js file. Removed shared.js, and while at it, removed jQuery as well.
    
    Now the extension works every time without manual refreshes

--- ganalytics-lastDay/background.js
+++ ganalytics-lastDay/background.js
@@ -1,6 +1,6 @@
 /*
-Google Analytics Last Day
-Copyright (C) 2014 Michele Bonazza <http://somethingididnotknow.wordpress.com>
+Google Analytics Today
+Copyright (C) 2013 Michele Bonazza <somethingididnotknow.wordpress.com>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -15,20 +15,32 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-var s = document.createElement('script');
-s.src = chrome.extension.getURL('shared.js');
 
-s.onload = function() {
-	this.parentNode.removeChild(s);
-};
-
-(document.head || document.documentElement).appendChild(s);
+function getUrl(code) {
+	// taken from http://stackoverflow.com/questions/7509010/bookmark-with-date-in-the-url/7509107#7509107
+	function pad2(n) {
+		var str = String(n);
+		if (str.length < 2)
+			str = "0" + str;
+		return str;
+	}
+	var date = new Date();
+	var today = '', yesterday = '';
+	today += date.getFullYear();
+	today += pad2(date.getMonth() + 1);
+	today += pad2(date.getDate());
+	date.setDate(date.getDate() - 1);
+	yesterday += date.getFullYear();
+	yesterday += pad2(date.getMonth() + 1);
+	yesterday += pad2(date.getDate());
+	return 'https://www.google.com/analytics/web/?#home/' + code +'/%3F_u.date00%3D' + yesterday + '%26_u.date01%3D' + today +'/=';
+}
 
 chrome.storage.local.get('analyticsID', function(stored) {
 	var id = stored.analyticsID;
   if (id === undefined) {
-    // if there's no stored ID, go the configuration page
-    window.location.href = 'conf.html';
+    	// if there's no stored ID, go the configuration page
+    	window.location.href = 'conf.html';
 	} else {
 		// otherwise, redirect to Google Analytics
 		document.getElementById('redirection').style.visibility = 'visible';
--- ganalytics-lastDay/conf.js
+++ ganalytics-lastDay/conf.js
@@ -1,6 +1,6 @@
 /*
-Google Analytics Last Day
-Copyright (C) 2014 Michele Bonazza <http://somethingididnotknow.wordpress.com>
+Google Analytics Today
+Copyright (C) 2013 Michele Bonazza <somethingididnotknow.wordpress.com>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -15,45 +15,57 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
+
+function getUrl(code) {
+  // taken from http://stackoverflow.com/questions/7509010/bookmark-with-date-in-the-url/7509107#7509107
+  function pad2(n) {
+    var str = String(n);
+    if (str.length < 2)
+      str = "0" + str;
+    return str;
+  }
+  
+  var date = new Date(), today = '', yesterday = '';
+  today += date.getFullYear();
+  today += pad2(date.getMonth() + 1);
+  today += pad2(date.getDate());
+  date.setDate(date.getDate() - 1);
+  yesterday += date.getFullYear();
+  yesterday += pad2(date.getMonth() + 1);
+  yesterday += pad2(date.getDate());
+  return 'https://www.google.com/analytics/web/?#home/' + code +'/%3F_u.date00%3D' + yesterday + '%26_u.date01%3D' + today +'/=';
+}
+    
 function interceptEnter() {
-	jQuery('input[type=text]').on('keydown', function(e) {
-		if (e.which === 13) {
-			e.preventDefault();
-			storeCode();
+  var el = document.getElementById("codeField");
+  
+	el.addEventListener('keydown', function(e) {
+    var key = e.which || e.keyCode;
+		if (key == 13) {
+      e.preventDefault();
+			storeCode(el.value);
 		}
 	});
 }
 
-function storeCode() {
-	var code = jQuery('#codeField').val();
+function storeCode(code) {
   chrome.storage.local.set({'analyticsID': code}, function() {
+    console.log('ID saved ');
     window.location.href = getUrl(code);
   });
 }
 
-// load shared js as a new script
-var s = document.createElement('script');
-s.src = chrome.extension.getURL('shared.js');
-s.onload = function() {
-	this.parentNode.removeChild(s);
-};
-(document.head || document.documentElement).appendChild(s);
-
-// use jquery because I'm lazy
-s = document.createElement('script');
-s.src = chrome.extension.getURL('jquery.min.js');
-s.onload = function() {
-	this.parentNode.removeChild(s);
-};
-(document.head || document.documentElement).appendChild(s);
-
 chrome.storage.local.get('analyticsID', function(stored) {
-	var id = stored.analyticsID;
+	var id = stored.analyticsID,
+    codeField = document.getElementById("codeField"),
+    sendButton = document.getElementById("sendButton");
+    
 	if (!id) {
-		jQuery("#codeField").attr('placeholder', 'Paste your code here');
+		codeField.setAttribute('placeholder', 'Paste your code here');
 	} else {
-		jQuery("#codeField").val(id)
+		codeField.value = id
 	}
-	document.querySelector('#sendButton').onclick = storeCode;
+  
+	sendButton.addEventListener('click', storeCode);
 	interceptEnter();
 });
